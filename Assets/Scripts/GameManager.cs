3c5e7ee17321cc976f47a39e7e0c756bd1d7df9d
Burak Gul <burakgl2465@gmail.com>
2024-05-09 18:52:02 +0300

    Objet pooling and obstacle design.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _ball;
+    [SerializeField] private GameObject[] _balls;
     [SerializeField] private GameObject _firePoint;
     [SerializeField] private float _ballPower;
+    private int _activeBallIndex;
     void Start()
     {
         
@@ -16,8 +17,17 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject ball = Instantiate(_ball, _firePoint.transform.position, _firePoint.transform.rotation);
-            ball.GetComponent<Rigidbody>().AddForce(ball.transform.TransformDirection(90,90,0) * _ballPower, ForceMode.Force);   
+            _balls[_activeBallIndex].transform.SetPositionAndRotation(_firePoint.transform.position, _firePoint.transform.rotation);
+            _balls[_activeBallIndex].SetActive(true);
+            _balls[_activeBallIndex].GetComponent<Rigidbody>().AddForce(_balls[_activeBallIndex].transform.TransformDirection(90,90,0) * _ballPower, ForceMode.Force);
+            if (_balls.Length - 1 == _activeBallIndex)
+            {
+                _activeBallIndex = 0;
+            }
+            else
+            {
+                _activeBallIndex++;
+            }
         }
     }
 }
